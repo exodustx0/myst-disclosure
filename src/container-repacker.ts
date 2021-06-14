exodustx0/myst-disclosure

commit f56dbd45fb7cc335a724707470345118ef73d665

Don't write some empty arrays to JSON

src/container-repacker.ts
@@ -155,32 +155,34 @@ export class ContainerRepacker {
 	}
 
 	private async readIndex() {
-		const index: Container.Index = {
-			dirs: [],
-			files: [],
-		};
-
 		const dir = (await fsP.readdir(this.sourcePath, { withFileTypes: true }))
 			.filter(entry => entry.isDirectory() || entry.isFile());
 
+		const subDirs: Container.DirInfo[] = [];
+		const files: Container.FileInfo[] = [];
 		for (const entry of dir) {
 			if (entry.isDirectory() && !entry.name.endsWith('-m4b')) {
 				this.path.push(entry.name);
-				index.dirs.push({
+				subDirs.push({
 					name: entry.name,
 					index: await this.readIndex(),
 				});
 				this.path.pop();
-			} else {
+			} else if (entry.isFile()) {
 				if (entry.name.endsWith('.log') && this.settings.skipLogFiles) continue;
 
-				index.files.push({
+				files.push({
 					name: entry.name,
 					size: 0,
 					offset: 0,
 				});
 			}
 		}
+
+		const index: Container.Index = {};
+
+		if (subDirs.length > 0) index.dirs = subDirs;
+		if (files.length > 0) index.files = files;
 
 		return index;
 	}
@@ -211,20 +213,28 @@ export class ContainerRepacker {
 	}
 
 	private async writeIndexToContainer(index: Container.Index, offset: number) {
-		await this.writeFile.writeUInt8(index.dirs.length);
-		for (const dirInfo of index.dirs) {
-			await this.writeFile.writeChar8Headered(dirInfo.name + '\0');
-			offset = await this.writeIndexToContainer(dirInfo.index, offset);
+		if (index.dirs) {
+			await this.writeFile.writeUInt8(index.dirs.length);
+			for (const dirInfo of index.dirs) {
+				await this.writeFile.writeChar8Headered(dirInfo.name + '\0');
+				offset = await this.writeIndexToContainer(dirInfo.index, offset);
+			}
+		} else {
+			await this.writeFile.writeUInt8(0);
 		}
 
-		await this.writeFile.writeUInt32(index.files.length);
-		for (const fileInfo of index.files) {
-			await this.writeFile.writeChar8Headered(fileInfo.name + '\0');
-			await this.writeFile.writeUInt32(fileInfo.size);
-			await this.writeFile.writeUInt32(offset);
-			offset += fileInfo.size;
-
-			if (this.settings.verbose) this.progressLogger!.tick();
+		if (index.files) {
+			await this.writeFile.writeUInt32(index.files.length);
+			for (const fileInfo of index.files) {
+				await this.writeFile.writeChar8Headered(fileInfo.name + '\0');
+				await this.writeFile.writeUInt32(fileInfo.size);
+				await this.writeFile.writeUInt32(offset);
+				offset += fileInfo.size;
+	
+				if (this.settings.verbose) this.progressLogger!.tick();
+			}
+		} else {
+			await this.writeFile.writeUInt32(0);
 		}
 
 		return offset;
@@ -357,14 +367,16 @@ export class ContainerRepacker {
 			await this.writeFile.writeUInt32(0x1); // unknown
 			await this.writeFile.writeCharEncHeadered(fileInfo.name.slice(0, -4));
 			await this.writeFile.writeUInt32(0x1); // unknown
-			await this.writeFile.writeUInt32(json.labels.length);
-			await this.writeFile.writeUInt32(json.groups.length);
-			await this.writeLabels(json.labels);
-			for (const group of json.groups) {
-				await this.writeFile.writeChar8Headered(group.name);
-				await this.writeFile.writeUInt32(group.labels.length);
-				await this.writeFile.writeUInt32(0x0); // unknown
-				await this.writeLabels(group.labels);
+			await this.writeFile.writeUInt32(json.labels ? json.labels.length : 0);
+			await this.writeFile.writeUInt32(json.groups ? json.groups.length : 0);
+			if (json.labels) await this.writeLabels(json.labels);
+			if (json.groups) {
+				for (const group of json.groups) {
+					await this.writeFile.writeChar8Headered(group.name);
+					await this.writeFile.writeUInt32(group.labels.length);
+					await this.writeFile.writeUInt32(0x0); // unknown
+					await this.writeLabels(group.labels);
+				}
 			}
 			
 			fileInfo.size = this.writeFile.bytesWritten;
@@ -410,16 +422,20 @@ export class ContainerRepacker {
 	}
 
 	private *files(index: Container.Index): Generator<Container.FileInfo> {
-		for (const dirInfo of index.dirs) {
-			this.path.push(dirInfo.name);
-			yield* this.files(dirInfo.index);
-			this.path.pop();
+		if (index.dirs) {
+			for (const dirInfo of index.dirs) {
+				this.path.push(dirInfo.name);
+				yield* this.files(dirInfo.index);
+				this.path.pop();
+			}
 		}
 
-		for (const fileInfo of index.files) {
-			this.path.push(fileInfo.name);
-			yield fileInfo;
-			this.path.pop();
+		if (index.files) {
+			for (const fileInfo of index.files) {
+				this.path.push(fileInfo.name);
+				yield fileInfo;
+				this.path.pop();
+			}
 		}
 	}
 

src/container-unpacker.ts
@@ -152,20 +152,17 @@ export class ContainerUnpacker {
 	}
 
 	private async readIndex() {
-		const index: Container.Index = {
-			dirs: [],
-			files: [],
-		};
-
 		const numSubDirs = await this.readFile.readUInt8();
+		const subDirs: Container.DirInfo[] = [];
 		for (let subDirIndex = 0; subDirIndex < numSubDirs; subDirIndex++) {
-			index.dirs.push({
+			subDirs.push({
 				name: (await this.readFile.readChar8Headered()).slice(0, -1),
 				index: await this.readIndex(),
 			});
 		}
 
 		const numFiles = await this.readFile.readUInt32();
+		const files: Container.FileInfo[] = [];
 		for (let fileIndex = 0; fileIndex < numFiles; fileIndex++) {
 			const fileInfo: Container.FileInfo = {
 				name: (await this.readFile.readChar8Headered()).slice(0, -1),
@@ -173,8 +170,13 @@ export class ContainerUnpacker {
 				offset: await this.readFile.readUInt32(),
 			};
 
-			if (!fileInfo.name.endsWith('.log') || !this.settings.skipLogFiles) index.files.push(fileInfo);
+			if (!fileInfo.name.endsWith('.log') || !this.settings.skipLogFiles) files.push(fileInfo);
 		}
+
+		const index: Container.Index = {};
+
+		if (subDirs.length > 0) index.dirs = subDirs;
+		if (files.length > 0) index.files = files;
 
 		return index;
 	}
@@ -302,11 +304,12 @@ export class ContainerUnpacker {
 			});
 		}
 
-		await this.writeToJSON<Labels.JSONFile>({
-			type: 'labels',
-			labels,
-			groups,
-		});
+		const json: Labels.JSONFile = { type: 'labels' };
+
+		if (labels.length > 0) json.labels = labels;
+		if (groups.length > 0) json.groups = groups;
+
+		await this.writeToJSON<Labels.JSONFile>(json);
 	}
 
 	private async readLabels(numLabels: number) {
@@ -361,16 +364,20 @@ export class ContainerUnpacker {
 	}
 
 	private *files(index: Container.Index): Generator<Container.FileInfo> {
-		for (const dirInfo of index.dirs) {
-			this.path.push(dirInfo.name);
-			yield* this.files(dirInfo.index);
-			this.path.pop();
+		if (index.dirs) {
+			for (const dirInfo of index.dirs) {
+				this.path.push(dirInfo.name);
+				yield* this.files(dirInfo.index);
+				this.path.pop();
+			}
 		}
 
-		for (const fileInfo of index.files) {
-			this.path.push(fileInfo.name);
-			yield fileInfo;
-			this.path.pop();
+		if (index.files) {
+			for (const fileInfo of index.files) {
+				this.path.push(fileInfo.name);
+				yield fileInfo;
+				this.path.pop();
+			}
 		}
 	}
 

src/types/container.d.ts
@@ -18,8 +18,8 @@ export interface JSONFile {
  * FileInfo | [ab] | file info structures         |
  */
 export interface Index {
-	dirs: DirInfo[];
-	files: FileInfo[];
+	dirs?: DirInfo[];
+	files?: FileInfo[];
 }
 
 /*

src/types/labels.d.ts
@@ -12,8 +12,8 @@
  */
 export interface JSONFile {
 	type: 'labels';
-	labels: Label[];
-	groups: Group[];
+	labels?: Label[];
+	groups?: Group[];
 }
 
 /*

src/util/container-helpers.ts
@@ -1,19 +1,19 @@
-import type { Index } from '../types/container.js';
+import type * as Container from '../types/container.js';
 
-export function numFilesInIndex(index: Index) {
+export function numFilesInIndex(index: Container.Index) {
 	let numFiles = 0;
 
-	for (const dirInfo of index.dirs) numFiles += numFilesInIndex(dirInfo.index);
-	numFiles += index.files.length;
+	if (index.dirs) for (const dirInfo of index.dirs) numFiles += numFilesInIndex(dirInfo.index);
+	if (index.files) numFiles += index.files.length;
 
 	return numFiles;
 }
 
-export function numBytesInIndex(index: Index) {
+export function numBytesInIndex(index: Container.Index) {
 	let numBytes = 5;
 
-	for (const dirInfo of index.dirs) numBytes += 5 + dirInfo.name.length + numBytesInIndex(dirInfo.index);
-	for (const fileInfo of index.files) numBytes += 13 + fileInfo.name.length;
+	if (index.dirs) for (const dirInfo of index.dirs) numBytes += 5 + dirInfo.name.length + numBytesInIndex(dirInfo.index);
+	if (index.files) for (const fileInfo of index.files) numBytes += 13 + fileInfo.name.length;
 
 	return numBytes;
 }

commit 9c56da59a7d7a97148f6faddedfe424a606c921f

Fix bug introduced in commit `f56dbd4`
Container repacker processes nested containers again

src/container-repacker.ts
@@ -175,7 +175,7 @@ export class ContainerRepacker {
 					index: await this.readIndex(),
 				});
 				this.path.pop();
-			} else if (entry.isFile()) {
+			} else if (entry.isFile() || (entry.isDirectory() && entry.name.endsWith('-m4b'))) {
 				if (entry.name.endsWith('.log') && this.settings.skipLogFiles) continue;
 
 				files.push({