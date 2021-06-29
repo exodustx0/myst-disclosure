21c86d7465162e225cec61383aa9f917b08fb393
Exodustx0 <m.v@exodu.st>
2021-06-29 16:33:22 +0200

    Put error messages in `errors.ts`

--- src/errors.ts
+++ src/errors.ts
@@ -2,11 +2,24 @@
 /* eslint-disable @typescript-eslint/indent */
 import type { Primitive } from 'type-fest';
 
-import { messages } from './messages.js';
+const messages = {
+	DESTINATION_INVALID: 'Destination must be a directory.',
+	FILE_CORRUPTED_OR_INVALID: '"{path}" is either corrupted or an invalid {type} file.',
+	FILE_UNEXPECTED_EXTENSION: 'Expected "{path}" to have .{extension} extension.',
+	JSON_INVALID: '"{path}" is not a valid {type} JSON file.',
+	NO_READ_PERMISSIONS_PATH: 'No read permissions for "{path}".',
+	NO_READ_PERMISSIONS_TYPE: 'No read permissions for {type} path.',
+	NO_WRITE_PERMISSIONS_PATH: 'No write permissions for "{path}".',
+	NO_WRITE_PERMISSIONS_TYPE: 'No write permissions for {type} path.',
+	PATH_DOES_NOT_EXIST: 'The {type} path does not exist.',
+	SOURCE_INVALID_REPACK: 'Source must be a -{extension} directory or a directory containing -{extension} directories.',
+	SOURCE_INVALID_UNPACK: 'Source must be a .{extension} file or a directory containing .{extension} files.',
+	TEMP_DIR_DELETE_FAILED: 'Temp directory "{path}" could not be deleted.',
+} as const;
 
 type Tokens<S extends string> = 
-	S extends `${string}{${infer Key}}${infer Rest}`
-	? Key | Tokens<Rest>
+	S extends `${string}{${infer Token}}${infer Rest}`
+	? Token | Tokens<Rest>
 	: never;
 
 type ReplacementObject<S extends string> =
